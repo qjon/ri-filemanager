d9a2e5a47ad01bdd042404eb0ddf706fd11b6d8a
Rafał Ignaszewski <rafal@ignaszewski.pl>
2015-07-28 08:31:15 +0200

    Update README.md StandAlone version

--- Resources/public/js/tinymce_plugin.js
+++ Resources/public/js/tinymce_plugin.js
@@ -8,7 +8,7 @@
  */
 function myFileBrowser(field_name, url, type, win) {
     tinyMCE.activeEditor.windowManager.open({
-        file: 'index.html',
+        file: '/app_dev.php/filemanager/page',
         title: 'RI FileManager',
         width: 1100,
         height: 450,
@@ -19,7 +19,21 @@ function myFileBrowser(field_name, url, type, win) {
     }, {
         window: win,
         input: field_name,
-        type: type
+        type: type,
+        oninsert: function(file) {
+            var inputs = win.document.getElementsByTagName('input');
+            if (type === 'image') {
+                inputs[0].setAttribute('value', file.src);
+                inputs[1].setAttribute('value', file.name);
+                inputs[2].setAttribute('value', file.width);
+                inputs[3].setAttribute('value', file.height);
+            } else {
+                inputs[0].setAttribute('value', file.src);
+                if (!inputs[1].getAttribute('value')) {
+                    inputs[1].setAttribute('value', file.name);
+                }
+            }
+        }
     });
     return false;
 }
